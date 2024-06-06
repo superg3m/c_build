e4898b89ae62ea986579623329d0b380ba1ed30a
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-05 23:19:08 -0400

    Update run.ps1

--- default/cl/run.ps1
+++ default/cl/run.ps1
@@ -27,3 +27,8 @@ param(
 	[string] $libs
 )
 
+./build.ps1 -executable_name $executable_name -compile_time_define $compile_time_define -std_version $std_version -debug_build $debug_build -generate_object_files $generate_object_files -include_paths $include_paths -source_paths $source_paths -lib_paths $lib_paths -libs $libs
+
+Push-Location ".\examples\cl"
+    & "./$executable_name"
+Pop-Location
