a43b18b3152ef32726b8de5bcd93b670e5c44cdc
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-06 16:36:03 -0400

    yep

--- default/cl/run.ps1
+++ default/cl/run.ps1
@@ -2,7 +2,7 @@ param(
 	[Parameter(Mandatory=$true)]
 	[string] $executable_name,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$false)]
 	[string] $compile_time_defines,
 
     [Parameter(Mandatory=$true)]
@@ -20,14 +20,23 @@ param(
     [Parameter(Mandatory=$true)]
 	[string] $source_paths,
 
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$false)]
 	[string] $lib_paths,
     
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$false)]
 	[string] $libs
 )
 
-./build.ps1 -executable_name $executable_name -compile_time_define $compile_time_define -std_version $std_version -debug_build $debug_build -generate_object_files $generate_object_files -include_paths $include_paths -source_paths $source_paths -lib_paths $lib_paths -libs $libs
+./build.ps1 `
+    -executable_name $executable_name `
+    -compile_time_define $compile_time_define `
+    -std_version $std_version `
+    -debug_build $debug_build `
+    -generate_object_files $generate_object_files `
+    -include_paths $include_paths `
+    -source_paths $source_paths `
+    -lib_paths $lib_paths `
+    -libs $libs
 
 Push-Location ".\examples\cl"
     & "./$executable_name"
