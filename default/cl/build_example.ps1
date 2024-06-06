63d44557b3776beacfcc6ba7490a830854de59b9
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-06 17:47:42 -0400

    Update build_example.ps1

--- default/cl/build_example.ps1
+++ default/cl/build_example.ps1
@@ -56,7 +56,7 @@ $timer = [Diagnostics.Stopwatch]::new() # Create a timer
 $timer.Start() # Start the timer
 
 Push-Location ".\examples\cl"
-    Write-Host "EXE: " $executable_name
+    Write-Host "EXE: " .\examples\cl..\$executable_name
     Invoke-Expression "$clCommand | Out-File -FilePath '../../compilation_errors.txt' -Append"
 Pop-Location
 
