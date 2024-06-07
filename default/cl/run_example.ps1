0af86e2c26f36cd6bc269f3420919e007691831b
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-06 22:51:28 -0400

    sim

--- default/cl/run_example.ps1
+++ default/cl/run_example.ps1
@@ -1,5 +1,8 @@
 param(
 	[Parameter(Mandatory=$true)]
+	[string] $lib_name,
+
+	[Parameter(Mandatory=$true)]
 	[string] $executable_name,
 
     [Parameter(Mandatory=$false)]
@@ -11,6 +14,9 @@ param(
     [Parameter(Mandatory=$true)]
 	[bool] $debug_build,
 
+	[Parameter(Mandatory=$true)]
+	[bool] $build_lib,
+
     [Parameter(Mandatory=$true)]
 	[bool] $generate_object_files,
 
@@ -18,19 +24,29 @@ param(
 	[string] $include_paths,
 
 	[Parameter(Mandatory=$true)]
+	[string] $source_paths,
+
+	[Parameter(Mandatory=$true)]
 	[string] $source_example_paths,
+
+	[Parameter(Mandatory=$false)]
+	[string] $additional_libs_for_build,
     
     [Parameter(Mandatory=$false)]
 	[string] $additional_libs_for_example
 )
 
 ./C-BUILD/default/cl/build_example.ps1 `
+	-lib_name $lib_name `
 	-executable_name $executable_name `
 	-compile_time_defines $compile_time_defines `
 	-std_version $std_version `
 	-debug_build $debug_build `
+	-build_lib $build_lib `
 	-include_paths $include_paths `
+	-source_paths $source_paths `
 	-source_example_paths $source_example_paths `
+	-additional_libs_for_build $additional_libs_for_build `
 	-additional_libs_for_example $additional_libs_for_example
 
 Push-Location ".\examples\cl"
--- templates/run_example.ps1
+++ templates/run_example.ps1
@@ -3,13 +3,17 @@
 $configPath = "config.json"
 $jsonData = Get-Content -Path $configPath -Raw | ConvertFrom-Json
 
+
+$lib_name = $jsonData.'$lib_name'
 $executable_name = $jsonData.'$executable_name'
 $compile_time_defines = $jsonData.'$compile_time_defines'
 $std_version = $jsonData.'$std_version'
 $debug_build = $jsonData.'$debug_build'
-$debug_build = $jsonData.'$debug_build'
+$build_lib = $jsonData.'$build_lib'
 $generate_object_files = $jsonData.'$generate_object_files'
-$source_example_paths = $jsonData.'$source_example_paths'
+$include_paths = $jsonData.'$include_paths'
+$source_paths = $jsonData.'$source_paths'
+$additional_libs_for_build = $jsonData.'$additional_libs_for_build'
 $additional_libs_for_example = $jsonData.'$additional_libs_for_example'
 
 Push-Location  ".\C-BUILD"
@@ -19,12 +23,16 @@ git pull
 Pop-Location
 
 ./C-BUILD/$preset/$compiler_type/run_example.ps1 `
+    -lib_name $lib_name `
     -executable_name $executable_name `
     -compile_time_defines $compile_time_defines `
     -std_version $std_version `
     -debug_build $debug_build `
+    -build_lib $build_lib `
     -generate_object_files $generate_object_files `
     -include_paths $include_paths `
+    -source_paths $source_paths `
     -source_example_paths $source_example_paths `
+    -additional_libs_for_build $additional_libs_for_build `
     -additional_libs_for_example $additional_libs_for_example
 
