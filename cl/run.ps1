4e257a5d6d0c28a2cbfe4d083cb16fbbb5b26df9
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-07 18:02:38 -0400

    Update run.ps1

--- cl/run.ps1
+++ cl/run.ps1
@@ -11,12 +11,13 @@ param(
 
 $jsonData = $build_json | ConvertFrom-Json
 
+$output_name = $jsonData.'$output_name'
 $build_name = $jsonData.'$build_name'
 
-./C-BUILD/default/cl/build.ps1 -build_json $jsonData
+./C-BUILD/default/cl/build.ps1 -build_json $build_json
 
 Write-Host "running [$project_name - $build_name] run.ps1..." -ForegroundColor Green
 
-Push-Location ".\build_cl"
-    & "./$executable_name"
+Push-Location $build_director
+    & "./$output_name"
 Pop-Location
