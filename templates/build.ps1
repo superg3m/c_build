0c407272e285913f2e9a494e14892652cdbbc2f5
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-07 15:10:20 -0400

    Update build.ps1

--- templates/build.ps1
+++ templates/build.ps1
@@ -20,8 +20,12 @@ $timer.Start() # Start the timer
 foreach ($key in $jsonData.PSObject.Properties.Name) {
     $value = $jsonData.$key # value is json
 
+    $should_run = $value.'$should_run'
 
-    
+    if ($should_run -eq $false) {
+        continue
+    }
+
     # If the value is an object, iterate over its properties as well
     if ($value -is [PSCustomObject]) {
 
@@ -32,12 +36,6 @@ foreach ($key in $jsonData.PSObject.Properties.Name) {
 
         foreach ($nestedKey in $value.PSObject.Properties.Name) {
             $nestedValue = $value.$nestedKey
-
-            $should_run = $value.'$should_run'
-
-            if ($should_run -eq $false) {
-                break
-            }
 
             if ($nestedValue -is [Array]) {
                 Write-Host ""
