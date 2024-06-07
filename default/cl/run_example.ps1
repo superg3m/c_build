a3b11f0125973817fe80ca7708d5acd1db3df95d
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-06 22:57:14 -0400

    Update run_example.ps1

--- default/cl/run_example.ps1
+++ default/cl/run_example.ps1
@@ -42,6 +42,7 @@ param(
 	-compile_time_defines $compile_time_defines `
 	-std_version $std_version `
 	-debug_build $debug_build `
+	-generate_object_files $generate_object_files `
 	-build_lib $build_lib `
 	-include_paths $include_paths `
 	-source_paths $source_paths `
