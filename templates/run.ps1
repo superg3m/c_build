3fe0b2bb75c19bd10c8fbd63e9a85cd090e85e97
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-07 18:16:33 -0400

    f

--- templates/run.ps1
+++ templates/run.ps1
@@ -8,13 +8,15 @@ $jsonData = Get-Content -Path $configPath -Raw | ConvertFrom-Json
 
 $project_name = $jsonData.'$project_name'
 
+Write-Host "running [$project_name] run.ps1..." -ForegroundColor Green
+
 Push-Location  ".\C-BUILD"
 git stash
 git stash drop
 git pull
 Pop-Location
 
-./build.ps1 -project_name $project_name -build_directory $key -build_json $jsonData -run_exe $true
+./build.ps1 -run_exe $true
 
 # run is complicated because what do you want to run?
 
