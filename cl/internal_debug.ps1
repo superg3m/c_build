bbba23da26249bb38ae3011b81e2256f4f411824
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-08 12:02:02 -0400

    Update internal_debug.ps1

--- cl/internal_debug.ps1
+++ cl/internal_debug.ps1
@@ -20,9 +20,11 @@ $output_name = $jsonData.'$output_name'
 Write-Host "running [$project_name - $build_procedure_name] debug.ps1..." -ForegroundColor Green
 
 ./c-build/cl/internal_build.ps1 -project_name $project_name -build_directory $build_directory -build_json $build_json -debug_build $true
-Push-Location $build_directory
 if ($debug_with_visual_studio -eq $true) {
     ./vars.ps1
+}
+Push-Location $build_directory
+if ($debug_with_visual_studio -eq $true) {
     devenv $output_name
 } else {
     & "raddbg" $output_name
