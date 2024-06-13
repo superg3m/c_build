9cb2caf0bcff2c0862e5fd1728165f151fac6ebc
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-12 21:48:56 -0400

    massive progress to detangling the mess

--- templates/new_build.ps1
+++ templates/new_build.ps1
@@ -4,9 +4,6 @@ $json_config_path = "c_build_config.json"
 
 $jsonData = Parse_JsonFile($json_config_path);
 
-
-Write-Host $jsonData.'$project_name'
-
 $project = [Project]::new($jsonData)
 
 $timer = Start_Timer $project.name
@@ -14,11 +11,8 @@ foreach ($key in $jsonData.PSObject.Properties.Name) {
     $value = $jsonData.$key
 
     if ($value -is [PSCustomObject]) {
-        #$json_convert = $value | ConvertTo-Json
-        #$build_procedure = [BuildProcedure]::new($json_convert)
-        #$project.AddBuildProcedure($build_procedure);
-
-        #$build_procedure.Print()
+        $build_procedure = [BuildProcedure]::new($key, $value)
+        $project.addBuildProcedure($build_procedure).InvokeBuild("$compiler_type")
     }
 }
 
--- utility/utils.ps1
+++ utility/utils.ps1
@@ -24,32 +24,61 @@ function Parse_JsonFile($file_path) {
     }
     $json_object = Get-Content -Path $file_path -Raw
 
-    return $json_object
+    return ConvertFrom-Json -InputObject $json_object
 }
 
 class BuildProcedure {
-    [bool]$should_build_procedure
-    
+    [string]$build_directory
+
     [string]$name
+
+    [bool]$should_build_procedure
+    [bool]$should_build_lib
+    [bool]$should_execute
+
     [string]$output_name
+    [string]$compile_time_defines
+    [string]$include_paths
+    [string]$source_paths
+    [string]$additional_libs
 
-    BuildProcedure ($json) {
-        $this.name = $json.'$build_procedure_name'
+    BuildProcedure ([string]$build_directory, [PSCustomObject]$jsonData) {
+        $this.build_directory = $build_directory
 
-        $this.should_build_procedure = $json.'$should_build_procedure'
+        $this.name = $jsonData.'$build_procedure_name'
 
-        $this.output_name = $json.'$output_name'
+        $this.should_build_procedure = $jsonData.'$should_build_procedure'
+        $this.should_build_lib = $jsonData.'$should_build_lib'
+        $this.should_execute = $jsonData.'$should_execute'
+
+        $this.output_name = $jsonData.'$output_name'
+        $this.compile_time_defines = $jsonData.'$compile_time_defines'
+        $this.include_paths = $jsonData.'$include_paths'
+        $this.source_paths = $jsonData.'$source_paths'
+        $this.additional_libs = $jsonData.'$additional_libs'
     }
 
     [void]InvokeBuild([string]$compiler_type) {
-        ../$compiler_type/internal_build.ps1
+        if ($this.should_build_procedure -eq $false) {
+            Write-Host "Skipping build procedure: $($this.name)" -ForegroundColor Magenta
+            continue
+        }
+
+        & "../$compiler_type/internal_build.ps1"
     }
 
     [void]Print() {
         Write-Host "================================================"
+        Write-Host "build_directory: $($this.build_directory)"
         Write-Host "name: $($this.name)"
         Write-Host "should_build_procedure: $($this.should_build_procedure)"
+        Write-Host "should_build_lib: $($this.should_build_lib)"
+        Write-Host "should_execute: $($this.should_execute)"
         Write-Host "output_name: $($this.output_name)"
+        Write-Host "compile_time_defines: $($this.compile_time_defines)"
+        Write-Host "include_paths: $($this.include_paths)"
+        Write-Host "source_paths: $($this.source_paths)"
+        Write-Host "additional_libs: $($this.additional_libs)"
         Write-Host "================================================"
     }
 }
@@ -63,36 +92,34 @@ class Project {
     [string[]]$project_dependencies_to_build
     [string]$std_version
 
-    [BuildProcedure[]]$BuildProcedures
+    [BuildProcedure[]]$build_procedures
 
-    Project ([string]$jsonData) {
-        Write-Host $jsonData.'$project_name'
-
-        #$this.name = $jsonData.'$project_name'
-
-        #Write-Host "sdfsdfd = $($this.name)" -ForegroundColor Red
-        #Write-Host "sdfsdfd = $( $json.'$project_name')" -ForegroundColor Red
+    Project ([PSCustomObject]$jsonData) {
+        $this.name = $jsonData.'$project_name'
 
         $this.debug_with_visual_studio = $jsonData.'$debug_with_visual_studio'
         $this.should_fully_rebuild_project_dependencies = $jsonData.'$should_fully_rebuild_project_dependencies'
 
         $this.project_dependencies_to_build = $jsonData.'$project_dependencies_to_build'
         $this.std_version = $jsonData.'$std_version'
+
+        $this.build_procedures = [System.Collections.ArrayList]@()
     }
 
-    [void]AddBuildProcedure([BuildProcedure]$build_proc) {
-        $this.BuildProcedures.Add($build_proc)
+    [BuildProcedure]addBuildProcedure([BuildProcedure]$build_proc) {
+        $this.build_procedures += $build_proc
+        return $build_proc
     }
 
     [void]Print() {
-        Write-Host "================== name: $($this.name) =================="
-        Write-Host "should_build_procedure: $($this.should_build_procedure)"
+        Write-Host "================== name: $($this.name) ==================" -ForegroundColor Magenta
         Write-Host "debug_with_visual_studio: $($this.debug_with_visual_studio)"
         Write-Host "should_fully_rebuild_project_dependencies: $($this.should_fully_rebuild_project_dependencies)"
         Write-Host "project_dependencies_to_build: $($this.project_dependencies_to_build)"
         Write-Host "std_version: $($this.std_version)"
-        foreach ($build_proc in $this.BuildProcedures) {
+        foreach ($build_proc in $this.build_procedures) {
             $build_proc.Print()
         }
+        Write-Host "================================================" -ForegroundColor Magenta
     }
 }
