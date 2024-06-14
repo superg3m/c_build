a7d2f0660e88877060296cdb03b6a2bda0427659
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-14 14:51:33 -0400

    Update decode_project.ps1

--- utility/decode_project.ps1
+++ utility/decode_project.ps1
@@ -18,23 +18,23 @@ $jsonData = Parse_JsonFile($json_config_path);
 
 $project = [Project]::new($jsonData, $compiler_override, $should_rebuild_project_dependencies_override)
 
+
+
+Push-Location  "./c-build"
+git fetch origin -q
+git reset --hard origin/main -q
+git pull -q
+Pop-Location
+
+foreach ($key in $jsonData.PSObject.Properties.Name) {
+    $value = $jsonData.$key
+
+    if ($value -is [PSCustomObject]) {
+        $build_procedure = [BuildProcedure]::new($key, $value)
+        $project.AddBuildProcedure($build_procedure)
+    }
+}
+
 $project.Print()
 
-# Push-Location  "./c-build"
-# git fetch origin -q
-# git reset --hard origin/main -q
-# git pull -q
-# Pop-Location
-# 
-# foreach ($key in $jsonData.PSObject.Properties.Name) {
-#     $value = $jsonData.$key
-# 
-#     if ($value -is [PSCustomObject]) {
-#         $build_procedure = [BuildProcedure]::new($key, $value)
-#         $project.AddBuildProcedure($build_procedure)
-#     }
-# }
-# 
-# # Write-Host $project -ForegroundColor Red
-# 
-# return $project
+return $project
