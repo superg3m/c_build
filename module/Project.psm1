208811b19ddf67f185dafde56914f9e6f2a7acc0
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-06-23 17:29:28 -0400

    Update Project.psm1

--- module/Project.psm1
+++ module/Project.psm1
@@ -1,4 +1,4 @@
-Import-Module "./c-build/module/sdfsdfsdf_Procedure.psm1 "
+Import-Module "./c-build/module/Procedure.psm1 "
 
 class Project {
     [string]$name
@@ -153,5 +153,3 @@ class Project {
         Write-Host "================================================" -ForegroundColor Magenta
     }
 }
-
-Export-ModuleMember -Class Project
