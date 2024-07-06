e9ed2f7c02c511a90d7f33739446962ce50229e2
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-07-06 11:01:58 -0400

    Update Procedure.py

--- scripts/Procedure.py
+++ scripts/Procedure.py
@@ -216,8 +216,8 @@ class Procedure:
         return self.build_no_check(debug)
 
     def clean(self):
+        FORMAT_PRINT(f"Cleaning: {self.build_directory}")
         for filename in os.listdir(self.build_directory):
-            FORMAT_PRINT(f"Cleaning: {self.build_directory}")
             file_path = os.path.join(self.build_directory, filename)
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
