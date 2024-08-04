468ed5505b8c34818e65d781b2e1f9a575377c5c
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-04 09:11:36 -0400

    Update compiler.py

--- scripts/compiler.py
+++ scripts/compiler.py
@@ -20,4 +20,25 @@
     # cc
     # clang
 
+class Compiler:
+    def __init__(self, compiler_json) -> None:
+        compiler_type = compiler_json["compiler type"]
+        compiler_version = compiler_json["compiler version"]
+        compiler_disable_warnings = compiler_json["compiler disable warnings"]
+        compile_time_defines = compiler_json["compile time defines"]
+
+        compiler_build_in_debug = compiler_json["compiler build in debug"]
+        compiler_build_static_lib = compiler_json["compiler build in debug"]
+        compiler_build_dynamic_lib = compiler_json["compiler build in debug"]
+
+        compiler_source_files = compiler_json["compiler source files"]
+        compiler_include_directories = compiler_json["compiler include directories"]
+        compiler_additional_lib_directories = compiler_json["compiler library directories"]
+        compiler_additional_libs = compiler_json["compiler libs"]
+
+        compiler_inject_into_args = compiler_json["compiler inject as argument"]
+
+        compiler_arguments = ""
+        # compiler type (cl, gcc, cc, clang)
+
 
