467160938583fa6ae6f5b9bddd8503edaceb0f29
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-04 09:02:30 -0400

    Create compiler.py

---
+++ scripts/compiler.py
@@ -0,0 +1,23 @@
+# Configuration
+    # address sanitizers
+    # disable warnings
+    # inject into compiler args
+    # specify version of c
+    # have default specification
+
+# init compiler
+    # setup compiler arguments
+    # source files
+    # include paths
+    # additional libs
+    # additional libs paths
+    # compile time defines
+    # invoke compiler
+
+#compilers to support
+    # gcc
+    # cl
+    # cc
+    # clang
+
+
