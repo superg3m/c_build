6a793727140e7a984c8ef85877ffb2a82dd9495f
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-04 14:27:50 -0400

    Better

--- scripts/compiler.py
+++ scripts/compiler.py
@@ -5,8 +5,9 @@
 # specify version of c
 # have default specification
 import os
-from typing import List
+from typing import List, Union, Dict
 
+from scripts.globals import FORMAT_PRINT
 
 # init compiler
 # setup compiler arguments
@@ -36,18 +37,101 @@ from typing import List
 # build with multi-threading!!!
 # build with multi-threading!!! /MP /WX
 
+from enum import Enum
+
+
+class CompilerType(Enum):
+    INVALID = -1
+    CL = 0
+    GCC = 1
+    CC = 2
+    CLANG = 3
+    COUNT = 4
+
+
+class CompilerAction(Enum):
+    NO_ACTION = -1
+    STD_VERSION = 0
+    NO_LOGO = 1
+    OBJECTS_ONLY = 2
+    COMPILE_TIME_DEFINES = 3
+    OUTPUT = 4
+    MULTI_THREADING = 5
+    ADDRESS_SANITIZER = 6
+    DISABLE_WARNINGS = 7
+    DISABLE_SPECIFIC_WARNING = 8
+    COUNT = 9
+
+
+compiler_lookup_table: List[List[str]] = [
+    # Compiler CL
+    [
+        "/std:",  # STD_VERSION
+        "/nologo",  # NO_LOGO
+        "/c",  # OBJECT_FLAG
+        "/Fe:",  # OUTPUT_FLAG
+        "/D",  # COMPILE_TIME_DEFINE_FLAG
+        "/MP",  # MULTI_THREADING
+        "/fsanitize=address",  # ADDRESS_SANITIZER
+        "/W0",  # DISABLE_WARNINGS
+        "/wd"  # DISABLE_SPECIFIC_WARNING
+    ],
+    # Compiler GCC
+    [
+        "-std=",  # STD_VERSION
+        None,  # NO_LOGO
+        "-c",  # OBJECT_FLAG
+        "-o",  # OUTPUT_FLAG
+        "-D",  # COMPILE_TIME_DEFINE_FLAG
+        "-j",  # MULTI_THREADING
+        "-fsanitize=address",  # ADDRESS_SANITIZER
+        "-w",  # DISABLE_WARNINGS
+        "-Wno-"  # DISABLE_SPECIFIC_WARNING
+    ],
+    # Compiler CC
+    [
+        "-std=",  # STD_VERSION
+        None,  # NO_LOGO
+        "-c",  # OBJECT_FLAG
+        "-o",  # OUTPUT_FLAG
+        "-D",  # COMPILE_TIME_DEFINE_FLAG
+        "-j",  # MULTI_THREADING
+        "-fsanitize=address",  # ADDRESS_SANITIZER
+        "-w",  # DISABLE_WARNINGS
+        "-Wno-"  # DISABLE_SPECIFIC_WARNING
+    ],
+    # Compiler CLANG
+    [
+        "-std=",  # STD_VERSION
+        None,  # NO_LOGO
+        "-c",  # OBJECT_FLAG
+        "-o",  # OUTPUT_FLAG
+        "-D",  # COMPILE_TIME_DEFINE_FLAG
+        "-j",  # MULTI_THREADING
+        "-fsanitize=address",  # ADDRESS_SANITIZER
+        "-w",  # DISABLE_WARNINGS
+        "-Wno-"  # DISABLE_SPECIFIC_WARNING
+    ]
+]
+
 
 class Compiler:
     def __init__(self, compiler_json, debug) -> None:
         # global
         self.compiler_type: str = compiler_json["compiler_type"]
+        self.compiler_type_enum: CompilerType = CompilerType.INVALID
+        self.compiler_action: CompilerAction = CompilerAction.NO_ACTION
+
+        self.compiler_type_enum = self.choose_compiler_type()
+        self.compiler_action: CompilerAction = CompilerAction.NO_ACTION
+
         self.std_version: str = compiler_json["std_version"]
         self.compiler_disable_warnings: List[str] = compiler_json["compiler_disable_warnings"]
         self.additional_libs: List[str] = compiler_json["additional_libs"]
         self.compiler_warning_level: str = compiler_json["compiler_warning_level"]
         self.compiler_treat_warnings_as_errors: bool = compiler_json["compiler_treat_warnings_as_errors"]
         self.compiler_inject_into_args = compiler_json["compiler inject as argument"]
-        self.compiler_debug: bool = debug
+        self.debug: bool = debug
 
         # procedure specific
         self.output_name: str = ""
@@ -86,33 +170,138 @@ class Compiler:
         self.include_paths = procedure_json["include_paths"]
         self.additional_libs = procedure_json["additional_libs"]
 
-    def get_compiler_index(self) -> int:
-        compiler_index: int = -1
-        if self.compiler_type == "cl":
-            compiler_index = 0
-        elif self.compiler_type == "gcc":
-            compiler_index = 1
-        elif self.compiler_type == "cc":
-            compiler_index = 2
-        elif self.compiler_type == "clang":
-            compiler_index = 3
-        return compiler_index
-
     def std_is_valid(self) -> bool:
-        compiler_index: int = self.get_compiler_index()
+        acceptable_versions: Dict[int, List[str]] = {
+            0: ["c11", "c17", "clatest"],  # CL
+            1: ["c89", "c90", "c99", "c11", "c17", "c18", "c23"],  # GCC
+            2: ["c89", "c90", "c99", "c11", "c17", "c18", "c23"],  # CC
+            3: ["c89", "c90", "c99", "c11", "c17", "c18", "c23"]  # CLANG
+        }
 
-        cl_lookup_table: List[str] = ["c11", "c17", "clatest"]
-        gcc_lookup_table: List[str] = ["c89", "c90", "c99", "c11", "c17", "c18", "c23"]
-        cc_lookup_table: List[str] = ["c89", "c90", "c99", "c11", "c17", "c18", "c23"]
-        clang_lookup_table: List[str] = ["c89", "c90", "c99", "c11", "c17", "c18", "c23"]
-        compiler_lookup_table: List[List[str]] = [
-            cl_lookup_table,
-            gcc_lookup_table,
-            cc_lookup_table,
-            clang_lookup_table
-        ]
+        return self.std_version in acceptable_versions[self.compiler_type_enum.value]
 
-        if self.std_version in compiler_lookup_table[compiler_index]:
-            return True
+    def choose_compiler_type(self):
+        temp = CompilerType.INVALID
+        if self.compiler_type == "cl":
+            temp = CompilerType.CL
+        elif self.compiler_type == "gcc":
+            temp = CompilerType.GCC
+        elif self.compiler_type == "cc":
+            temp = CompilerType.CC
+        elif self.compiler_type == "clang":
+            temp = CompilerType.CLANG
+        return temp
+
+    def set_action(self, action: CompilerAction):
+        self.compiler_action = action
+
+    def get_compiler_lookup(self) -> str:
+        return compiler_lookup_table[self.compiler_type_enum.value][self.compiler_action.value]
+
+    def invoke(self):
+        compiler_command: List[str] = [self.compiler_type]
+
+        for source in self.source_files:
+            if source:
+                compiler_command.append(source)
+
+        # Add no logo flag
+        self.set_action(CompilerAction.NO_LOGO)
+        no_logo_flag = self.get_compiler_lookup()
+        if no_logo_flag:
+            compiler_command.append(no_logo_flag)
+
+        # Add std version flag
+        self.set_action(CompilerAction.STD_VERSION)
+        std_version_flags = self.get_compiler_lookup()
+        if self.std_is_valid():
+            compiler_command.append(f"{std_version_flags}{self.std_version}")
         else:
-            return False
+            FORMAT_PRINT(f"Std version: {self.std_version} not supported, falling back on default")
+
+        # Add compile time defines
+        self.set_action(CompilerAction.COMPILE_TIME_DEFINES)
+        compile_time_define_flag = self.get_compiler_lookup()
+        for define in self.compile_time_defines:
+            if define:
+                compiler_command.append(f"{compile_time_define_flag}{define}")
+
+        # Add object flag
+        if self.should_build_static_lib:
+            self.set_action(CompilerAction.OBJECTS_ONLY)
+            object_flag = self.get_compiler_lookup()[
+                CompilerAction.OBJECTS_ONLY.value]  # Get the object flag for the specific compiler
+            compiler_command.append(object_flag)
+        else:
+            if self.should_build_dynamic_lib:
+                dynamic_lib_flag = self.get_compiler_lookup()[
+                    CompilerAction.OBJECTS_ONLY.value]  # Use the appropriate flag for dynamic libs
+                if self.compiler_type == "cl":
+                    compiler_command.append("/LD")  # Use CL-specific flag for dynamic libraries
+                else:
+                    compiler_command.append("-shared")  # Use common flag for GCC/CC/CLANG
+
+            # Add output flag
+            self.set_action(CompilerAction.OUTPUT)
+            output_flag = self.get_compiler_lookup()[CompilerAction.OUTPUT.value]
+            compiler_command.append(output_flag)
+            compiler_command.append(self.output_name)
+
+            if self.additional_libs:
+                for lib in self.additional_libs:
+                    if lib:
+                        compiler_command.append(lib)
+
+        # Add multi-threading flag
+        self.set_action(CompilerAction.MULTI_THREADING)
+        multi_threading_flag = self.get_compiler_lookup()
+        compiler_command.append(multi_threading_flag)
+
+        # Add address sanitizer flag
+        self.set_action(CompilerAction.ADDRESS_SANITIZER)
+        address_sanitizer_flag = self.get_compiler_lookup()
+        compiler_command.append(address_sanitizer_flag)
+
+        # Add disable warnings flag
+        self.set_action(CompilerAction.DISABLE_WARNINGS)
+        disable_warnings_flag = self.get_compiler_lookup()
+        compiler_command.append(disable_warnings_flag)
+
+        # Add disable specific warning flag
+        self.set_action(CompilerAction.DISABLE_SPECIFIC_WARNING)
+        disable_specific_warning_flag = self.get_compiler_lookup()
+        specific_warning = "4100"  # Replace with the warning number you want to disable
+        compiler_command.append(f"{disable_specific_warning_flag}{specific_warning}")
+
+        # Add warnings as errors flag
+        if self.compiler_treat_warnings_as_errors:
+            if self.compiler_type == "cl":
+                compiler_command.append("/WX")
+            else:
+                compiler_command.append("-Werror")
+
+        # Add optimization flag
+        if self.debug:
+            if self.compiler_type == "cl":
+                compiler_command.append("/Od")
+                compiler_command.append("/Zi")
+            else:
+                compiler_command.append("-g")
+        else:
+            if self.compiler_type == "cl":
+                compiler_command.append("/O2")
+            else:
+                compiler_command.append("-O2")
+
+        # Add include paths
+        for include_path in self.include_paths:
+            if include_path:
+                if self.compiler_type == "cl":
+                    compiler_command.append(f"/I{include_path}")
+                else:
+                    compiler_command.append(f"-I{include_path}")
+
+        # Add additional compiler flags
+        for flag in self.compiler_inject_into_args:
+            if flag:
+                compiler_command.append(flag)
