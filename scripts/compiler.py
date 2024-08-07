2d461458c6d9e0f59336aba4ae3bdd81f72860f2
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-07 13:16:32 -0400

    Update compiler.py

--- scripts/compiler.py
+++ scripts/compiler.py
@@ -253,7 +253,7 @@ class Compiler:
             if lib:
                 if self.compiler_type_enum == CompilerType.GCC_CC_CLANG:
                     GCC_CC_CLANG_path = lib[:lib.rindex("/") + 1]
-                    GCC_CC_CLANG_lib = lib.replace(".lib", "").replace(".dll", "").replace(".dylib", "")
+                    GCC_CC_CLANG_lib = lib.replace(".lib", "").replace(".dll", "").replace(".dylib", "").replace(".a", "")
                     GCC_CC_CLANG_lib = GCC_CC_CLANG_lib.replace(GCC_CC_CLANG_path, "")
                     compiler_command.append(f"-L{GCC_CC_CLANG_path}")
                     compiler_command.append(f" -l{GCC_CC_CLANG_lib}")
