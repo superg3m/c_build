8c1ae4f0b362e79d6db50f290f66291e8354b4f9
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-13 18:03:07 -0400

    Update validate_temp_files.ps1

--- validate_temp_files.ps1
+++ validate_temp_files.ps1
@@ -8,6 +8,11 @@ $current_content = Get-Content -Path $current_filename
 $differences = Compare-Object -ReferenceObject $current_content -DifferenceObject $template_content
 
 if ($differences) {
-    Write-Host "Template content for '$current_filename' is out of sync; you must run ./bootstrap.ps1" -ForegroundColor Red
+    if ($current_filename -eq "bootstrap.ps1") {
+        Write-Host "Template content for '$current_filename' is out of sync; you must run ./c-build/bootstrap.ps1" -ForegroundColor Red
+    } else {
+        Write-Host "Template content for '$current_filename' is out of sync; you must run ./bootstrap.ps1" -ForegroundColor Red
+    }
+
     exit
 }
