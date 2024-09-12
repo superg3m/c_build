16c3ea102258a38be5a9a444828a76b53aef05ce
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-12 12:35:53 -0400

    Update globals.py

--- new_stuff/globals.py
+++ new_stuff/globals.py
@@ -95,7 +95,7 @@ def build_static_lib(compiler_name, output_name, additional_libs):
     elif compiler_name in ["gcc", "cc", "clang"] and sys.platform == 'darwin':
         lib_command = [
             "ar",
-            "-static -r -o",
+            "-r -o",
             output_name,
         ] + object_files
     else:
