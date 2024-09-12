19fe5bdc4e423ebbad825986a8708d60d6571115
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-11 22:35:18 -0400

    Update new_project.py

--- new_stuff/new_project.py
+++ new_stuff/new_project.py
@@ -88,6 +88,7 @@ class Project:
 
             cached_current_directory_global = os.getcwd()
             os.chdir(dependency_string)
+            GIT_PULL("c_build")
             subprocess.call(f"python -B -m c_build_script --compiler {self.compiler_name} --build_type {build_type} --level {str(GET_LEVEL())}", shell=True)
             os.chdir(cached_current_directory_global)
 
