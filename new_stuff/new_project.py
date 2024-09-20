7a42f441a2fc168134bb1df401c22ac560b8e815
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-20 19:37:46 -0400

    Update new_project.py

--- new_stuff/new_project.py
+++ new_stuff/new_project.py
@@ -145,6 +145,7 @@ class Project:
     def set_rebuild_project_dependencies(self, should_rebuild_project_dependencies):
         print("BEFORE ASSIGNMENT: ", os.getenv("SHOULD_REBUILD", "DONT HAVE IT"))
         self.should_rebuild_project_dependencies = bool(os.getenv("SHOULD_REBUILD", should_rebuild_project_dependencies))
+        print("AFTER ASSIGNMENT: ", self.should_rebuild_project_dependencies)
 
     def set_debug_with_visual_studio(self, should_debug_with_visual_studio):
         self.should_debug_with_visual_studio = should_debug_with_visual_studio
