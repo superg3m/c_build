06f389f269a80179546ecf897bf0f5d1c8b64811
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-20 19:25:06 -0400

    Update new_project.py

--- new_stuff/new_project.py
+++ new_stuff/new_project.py
@@ -119,7 +119,7 @@ class Project:
         os.chdir(true_cached)
 
         for proc in self.procedures:
-            print(self.is_dependency, self.name)
+            print(self.__check_procedure_built(proc), " | ", self.is_dependency, " | ", self.name, " | ", self.should_rebuild_project_dependencies)
             if self.__check_procedure_built(proc) and self.is_dependency and not self.should_rebuild_project_dependencies:
                 NORMAL_PRINT(f"Already built procedure: {os.path.join(proc.build_directory,proc.output_name)}, skipping...")
                 continue
