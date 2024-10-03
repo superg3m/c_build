e4b97cb26425f76b1dbfa802288d6bd650f8cca6
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-10-03 17:42:06 -0400

    Update Utilities.py

--- source/Utilities.py
+++ source/Utilities.py
@@ -64,6 +64,7 @@ def __IS_PULL_REQUIRED(path: str) -> bool:
 git_had_to_pull = []
 def GIT_PULL(path: str):
     global git_had_to_pull
+    FATAL_PRINT(path)
     if not __IS_PULL_REQUIRED(path):
         return
 
