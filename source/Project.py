fc9409883d8fdbd8e24ff769ce70c3e51b76644b
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-10-02 22:46:19 -0400

    Update Project.py

--- source/Project.py
+++ source/Project.py
@@ -1,14 +1,15 @@
 import os
 import time
+from typing import Dict
 
 from .Utilities import NORMAL_PRINT, FORMAT_PRINT, DOWN_LEVEL, C_BUILD_EXECUTION_TYPE, UP_LEVEL
 
 class Project:
-    def __init__(self, project_config, procedures_config, is_dependency = False,):
+    def __init__(self, project_config: Dict, procedures_config: Dict, is_dependency = False,):
         self.project_name = project_config["project_name"]
         self.project_debug_with_visual_studio = project_config["project_debug_with_visual_studio"]
         self.project_executable_procedures = project_config["project_executable_procedures"]
-        self.procedures = [procedure_data for procedure_key, procedure_data in procedures_config]
+        self.procedures = [procedure_data for procedure_data in procedures_config.values()]
         self.is_dependency = is_dependency
 
     def __check_procedure_built(self, build_dir, output_name):
