a8a351ed22e9507f918d8d719b9af82d5dd8d858
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-10-04 12:00:58 -0400

    Update Procedure.py

--- source/Procedure.py
+++ source/Procedure.py
@@ -1,6 +1,7 @@
 import os
+import re
 import subprocess
-from typing import Dict, re
+from typing import Dict
 
 from .Utilities import IS_WINDOWS, FATAL_PRINT, RESOLVE_FILE_GLOB, FORMAT_PRINT, IS_WINDOWS_PROCESS_RUNNING, NORMAL_PRINT
 
