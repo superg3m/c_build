5f6a52242384ea643418866757ee626606cc7fe5
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-10-04 11:38:52 -0400

    Update Project.py

--- source/Project.py
+++ source/Project.py
@@ -17,7 +17,7 @@ class Project:
         self.should_rebuild = project_config.get("project_rebuild_project_dependencies", False)
         self.executable_procedures_names = project_config["project_executable_procedures"]
         self.procedures = [Procedure(MANAGER_COMPILER, procedure_data) for procedure_data in procedures_config.values()]
-        self.project_executable_procedures = [proc for proc in self.procedures if any(proc.output_name == name for name in self.executable_procedures_names)]
+        self.project_executable_procedures = [proc for proc in self.procedures if any(name in proc.output_name for name in self.executable_procedures_names)]
 
         self.is_dependency = is_dependency
         self.project_config = project_config
