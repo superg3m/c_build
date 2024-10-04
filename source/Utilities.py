d74713bf457646bb0425ce2dd6a8ee35e3f92724
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-10-04 09:54:36 -0400

    Update Utilities.py

--- source/Utilities.py
+++ source/Utilities.py
@@ -45,14 +45,14 @@ def __IS_PULL_REQUIRED(path: str) -> bool:
     return False
 
 git_had_to_pull = []
-def GIT_PULL(path: str, dep):
+def GIT_PULL(path: str):
     global git_had_to_pull
     if not __IS_PULL_REQUIRED(path):
         return
 
     if "c_build" not in path:
-        for i in range(len(dep)):
-            git_had_to_pull.append(True)
+        git_had_to_pull.append(True)
+
 
     cache_dir = os.getcwd()
     os.chdir(path)
