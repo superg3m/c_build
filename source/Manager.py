3e288b06f918d28d9b41a98772a9f81240968ef8
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-10-26 22:07:44 -0400

    Update Manager.py

--- source/Manager.py
+++ source/Manager.py
@@ -19,7 +19,7 @@ class Manager:
     def build_project(self):
         serialized_name = f"c_build_dependency_cache_{C_BUILD_COMPILER_NAME()}.json"
         for dependency_name in self.project_config["project_dependencies"]:
-            if dependency_name:
+            if dependency_name and os.path.exists(f"./{dependency_name}"):
                 if GIT_PULL(dependency_name) and os.path.exists(f"./{dependency_name}/{serialized_name}"):
                     os.remove(f"./{dependency_name}/{serialized_name}")
 
