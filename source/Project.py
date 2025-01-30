65e5da63b15fb08dfc6c4c7928e3ac6e356ab6a0
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-01-29 20:00:16 -0500

    Ok that was a bad idea

--- source/Project.py
+++ source/Project.py
@@ -123,8 +123,7 @@ class Project:
         for proc in self.procedures:
             if not self.project_rebuild_project_dependencies:
                 if (self.__check_procedure_built(proc.build_directory, proc.output_name) and
-                    self.is_dependency and PEEK_GIT_PULL() == False and
-                    self.MANAGER_COMPILER.compiler_disable_sanitizer == True):
+                    self.is_dependency and PEEK_GIT_PULL() == False):
                     NORMAL_PRINT(f"Already built procedure: {os.path.join(proc.build_directory, proc.output_name)}, skipping...")
                     continue
 
