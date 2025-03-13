cf24d7a7000e82bc95386d839fe94ca2eb50ce42
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-13 17:42:17 -0400

    Update Manager.py

--- source/Manager.py
+++ source/Manager.py
@@ -21,6 +21,8 @@ class Manager:
         self.procedures_config = procedures_config
 
     def build_project(self):
+        FATAL_PRINT(f"Project: {self.project_config.project_name} | Compiler: {C_BUILD_COMPILER_NAME()}")
+        FATAL_PRINT(f"Project: {self.project_config.project_name} | Compiler: {self.INTERNAL_COMPILER.cc.compiler_name}")
         serialized_name = f"c_build_dependency_cache_{C_BUILD_COMPILER_NAME()}.json"
         for dependency_name in self.project_config.project_dependencies:
             if dependency_name and os.path.exists(f"./{dependency_name}"):
