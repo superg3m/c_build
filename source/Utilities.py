c3ab5e64aa2a6b6586a6cda0d43212c3ba02c91a
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-13 18:16:37 -0400

    Update Utilities.py

--- source/Utilities.py
+++ source/Utilities.py
@@ -24,7 +24,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--build_type', default="debug", type=str, required=False, help='build_type -> { debug, release }')
 parser.add_argument('--is_dependency', default="false", type=str, required=False, help='is_dependency -> { true, false }')
 parser.add_argument('--execution_type', default="BUILD", type=str, required=False, help='Build type -> { BUILD, RUN, CLEAN, DEBUG }')
-parser.add_argument('--compiler_name', default="cl", type=str, required=False, help='Compiler Name -> { cl, gcc, cc, clang }')
+parser.add_argument('--compiler_name', default="INVALID_COMPILER", type=str, required=False, help='Compiler Name -> { cl, gcc, cc, clang }')
 
 import json
 
