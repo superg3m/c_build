d17dccbe23372680f06a95cde31092247b98f760
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-13 16:25:10 -0400

    Update Manager.py

--- source/Manager.py
+++ source/Manager.py
@@ -20,6 +20,20 @@ class Manager:
         self.project_config = project_config
         self.procedures_config = procedures_config
 
+    def serialize_into_json(self, serialized_name):
+        filtered_project_config = self.project_config.to_dict()
+        filtered_procedure_config = {}
+        for key, value in self.procedures_config.items():
+            filtered_procedure_config[key] = value.to_dict()
+
+        serialized_data = {
+            **filtered_project_config,
+            **filtered_procedure_config
+        }
+        with open(serialized_name, "w") as file:
+            json.dump(serialized_data, file, indent=4)
+        return
+
     def build_project(self):
         serialized_name = f"c_build_dependency_cache_{C_BUILD_COMPILER_NAME()}.json"
 
@@ -31,18 +45,7 @@ class Manager:
                 os.remove(f"./{dependency_name}/{serialized_name}")
 
         if C_BUILD_IS_DEPENDENCY() and not os.path.exists(serialized_name):
-            filtered_project_config = self.project_config.to_dict()
-            filtered_procedure_config = {}
-            for key, value in self.procedures_config.items():
-                filtered_procedure_config[key] = value.to_dict()
-
-            serialized_data = {
-                **filtered_project_config,
-                **filtered_procedure_config
-            }
-            with open(serialized_name, "w") as file:
-                json.dump(serialized_data, file, indent=4)
-            return
+            self.serialize_into_json(serialized_name)
 
         project = Project(self.INTERNAL_COMPILER, self.project_config, self.procedures_config)
         project.build()
