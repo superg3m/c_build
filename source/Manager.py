e1e85684723a7770758caa9bd027d6dfd6ef471a
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-13 15:21:20 -0400

    Update Manager.py

--- source/Manager.py
+++ source/Manager.py
@@ -29,7 +29,7 @@ class Manager:
             if os.path.exists(f"./{dependency_name}/{serialized_name}") and IS_PULL_REQUIRED(dependency_name):
                 os.remove(f"./{dependency_name}/{serialized_name}")
 
-        if C_BUILD_IS_DEPENDENCY() and not os.path.exists(serialized_name):
+        if C_BUILD_IS_DEPENDENCY() and os.path.exists(serialized_name):
             filtered_project_config = self.project_config.to_dict()
             filtered_procedure_config = {}
             for key, value in self.procedures_config.items():
