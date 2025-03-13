f0137da516391946722ac60ed72a956b7970efa6
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-13 15:29:53 -0400

    Update Project.py

--- source/Project.py
+++ source/Project.py
@@ -75,9 +75,8 @@ class Project:
                     os.system(f"git clone https://github.com/superg3m/c_build.git")
                     os.chdir(cache_dir)
                 else:
-                    pass
-                    # GIT_PULL(dependency)
-                    # GIT_PULL(f"{dependency}/c_build")
+                    GIT_PULL(dependency)
+                    GIT_PULL(f"{dependency}/c_build")
 
                 cache_dir = os.getcwd()
                 os.chdir(dependency)
