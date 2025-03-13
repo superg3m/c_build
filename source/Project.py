2f5aba385c6a7285b6114bdde6433d4c1a3316fa
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-13 19:09:58 -0400

    ok

--- source/Project.py
+++ source/Project.py
@@ -71,11 +71,6 @@ class Project:
                 if not os.path.exists(dependency):
                     FORMAT_PRINT(f"missing {dependency} cloning...")
                     os.system(f"git clone {github_root}/{dependency}.git")
-                else:
-                    GIT_PULL(dependency) # Optional Eventually!
-                    serialized_name = f"c_build_dependency_cache_{self.MANAGER_COMPILER.cc.compiler_name}.json"
-                    if PEEK_GIT_PULL():
-                        os.remove(f"./{dependency}/{serialized_name}")
 
                 cache_dir = os.getcwd()
                 os.chdir(dependency)
