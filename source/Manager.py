3f633ac0be83e773a15f9f54d1a0dbb7c938f6e2
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-13 16:59:09 -0400

    Update Manager.py

--- source/Manager.py
+++ source/Manager.py
@@ -21,14 +21,11 @@ class Manager:
         self.procedures_config = procedures_config
 
     def build_project(self):
-        serialized_name = f"c_build_dependency_cache_{self.INTERNAL_COMPILER.cc.compiler_name}.json"
-
+        serialized_name = f"c_build_dependency_cache_{C_BUILD_COMPILER_NAME()}.json"
         for dependency_name in self.project_config.project_dependencies:
-            if not dependency_name:
-                continue
-
-            if os.path.exists(f"./{dependency_name}/{serialized_name}") and IS_PULL_REQUIRED(dependency_name):
-                os.remove(f"./{dependency_name}/{serialized_name}")
+            if dependency_name and os.path.exists(f"./{dependency_name}"):
+                if os.path.exists(f"./{dependency_name}/{serialized_name}") and IS_PULL_REQUIRED(dependency_name):
+                    os.remove(f"./{dependency_name}/{serialized_name}")
 
         if C_BUILD_IS_DEPENDENCY():
             filtered_project_config = self.project_config.to_dict()
