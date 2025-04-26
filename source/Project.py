d3a1d3243865f836e90b8388601f2fa4acc3dbc4
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-04-26 13:55:27 -0400

    Update Project.py

--- source/Project.py
+++ source/Project.py
@@ -119,9 +119,9 @@ class Project(ProjectConfig):
         self.build_dependencies(self.pc)
 
         for proc in self.procedures:
-            if (not self.project_rebuild_project_dependencies or
-                (self.MANAGER_COMPILER.compiler_disable_sanitizer
-                 and self.build_type == "debug")):
+            if (self.project_rebuild_project_dependencies == False or
+                (self.MANAGER_COMPILER.compiler_disable_sanitize == False and
+                 self.build_type == "debug")):
                 if (self.__check_procedure_built(proc.build_directory, proc.output_name) and
                     self.is_dependency and PEEK_GIT_PULL() == False and
                     (not self.MANAGER_COMPILER.compiler_disable_sanitizer and self.build_type == "debug")):
