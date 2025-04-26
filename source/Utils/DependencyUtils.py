ca6ef32437cf40f195ca96d0a0c8b30f31ad9e7c
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-04-26 11:06:31 -0400

    yerp

--- source/Utils/DependencyUtils.py
+++ source/Utils/DependencyUtils.py
@@ -6,17 +6,17 @@ parser.add_argument('--is_dependency', default="false", type=str, required=False
 parser.add_argument('--execution_type', default="BUILD", type=str, required=False, help='Build type -> { BUILD, RUN, CLEAN, DEBUG }')
 parser.add_argument('--compiler_name', default="INVALID_COMPILER", type=str, required=False, help='Compiler Name -> { cl, gcc, cc, clang }')
 
-def C_BUILD_BUILD_TYPE():
+def C_BUILD_BUILD_TYPE() -> str:
     return parser.parse_args().build_type
 
-def C_BUILD_IS_DEBUG():
-    return parser.parse_args().build_type == "debug"
+def C_BUILD_EXECUTION_TYPE() -> str:
+    return parser.parse_args().execution_type
 
-def C_BUILD_COMPILER_NAME():
+def C_BUILD_COMPILER_NAME() -> str:
     return parser.parse_args().compiler_name
 
-def C_BUILD_IS_DEPENDENCY():
-    return parser.parse_args().is_dependency == "true"
+def C_BUILD_IS_DEBUG() -> bool:
+    return parser.parse_args().build_type == "debug"
 
-def C_BUILD_EXECUTION_TYPE():
-    return parser.parse_args().execution_type
+def C_BUILD_IS_DEPENDENCY() -> bool:
+    return parser.parse_args().is_dependency == "true"
