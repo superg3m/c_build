69d926e977e1a58b78c1a97083404c35e91b8c59
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-05-06 23:22:29 -0400

    Update Manager.py

--- source/Manager.py
+++ source/Manager.py
@@ -27,12 +27,14 @@ class Manager:
         for key, value in self.procedures.items():
             filtered_procedure_config[key] = value.to_dict()
 
-        print(self.pc.to_dict())
+
 
         serialized_data = {
             **self.pc.to_dict(),
             **filtered_procedure_config
         }
+
+        print(serialized_data)
 
         with open(serialized_name, "w") as file:
             json.dump(serialized_data, file, indent=4)
