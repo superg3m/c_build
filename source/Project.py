d964f470d6d9f81599f12594fbf3e6c4af4316af
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-05-06 22:15:35 -0400

    Update Project.py

--- source/Project.py
+++ source/Project.py
@@ -71,7 +71,6 @@ class Project(ProjectConfig):
 
             if not os.path.exists(dependency.name):
                 FORMAT_PRINT(f"missing {dependency.name} cloning...")
-                FATAL_PRINT(f"git clone -b {dependency.branch_name} {dependency.host}/{dependency.name}")
                 os.system(f"git clone -b {dependency.branch_name} {dependency.host}/{dependency.name}")
 
             cache_dir = os.getcwd()
