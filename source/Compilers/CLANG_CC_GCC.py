0c6de871d557503ad53254d98b83ad34fc85e914
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-05-07 14:46:10 -0400

    Update CLANG_CC_GCC.py

--- source/Compilers/CLANG_CC_GCC.py
+++ source/Compilers/CLANG_CC_GCC.py
@@ -177,10 +177,10 @@ class CLANG_GCC_Compiler(CompilerConfig):
             result = subprocess.run(compiler_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
             for line in result.stdout.splitlines():
-                NORMAL_PRINT(line.strip())
+                WARN_PRINT(line.strip())
 
             for line in result.stderr.splitlines():
-                NORMAL_PRINT(line.strip())
+                WARN_PRINT(line.strip())
 
             FORMAT_PRINT(f'cd "{build_directory}" ; {" ".join(compiler_command)}', end="",)
 
