2d3f75d7d60cc7e4477a0583d4fd8050ff9446f9
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-05-07 14:40:54 -0400

    intesting

--- source/Utils/InternalUtilities.py
+++ source/Utils/InternalUtilities.py
@@ -101,7 +101,7 @@ def build_static_lib(compiler_name, output_name, additional_libs):
         for line_2 in result.stderr.splitlines():
             NORMAL_PRINT(line_2.strip())
 
-        FORMAT_PRINT(f' ; {" ".join(lib_command)} ;')
+        FORMAT_PRINT(f'; {" ".join(lib_command)}')
     except FileNotFoundError:
         FATAL_PRINT(f"{lib_command[0]} command not found")
         error_occurred = True
