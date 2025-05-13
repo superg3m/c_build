67b28491bf3d6a05964cbdff93cd544dc21e8051
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-05-12 22:41:38 -0400

    s

--- source/Project.py
+++ source/Project.py
@@ -156,6 +156,7 @@ class Project(ProjectConfig):
 
             already_built = self.__check_procedure_built(proc.build_directory, proc.output_name)
             no_git_changes = PEEK_GIT_PULL() == False
+            print(git_had_to_pull)
             NORMAL_PRINT(f"NAME: {self.project_name} | NO_PULL: {no_git_changes}")
             if already_built and self.is_dependency and no_git_changes:
                 proc_name = os.path.join(proc.build_directory, proc.output_name)
