f20f44bace7c963d988d2e29aeb43840179a8ee8
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-08-03 17:52:39 -0400

    Update UserUtilities.py

--- source/UserUtilities.py
+++ source/UserUtilities.py
@@ -8,3 +8,9 @@ def GET_LIB_NAME(cc: CompilerConfig, lib_name: str) -> str:
 
 def GET_LIB_FLAG(cc: CompilerConfig, lib_name: str) -> str:
     return f"{lib_name}.lib" if cc.compiler_name == "cl" else f"-l{lib_name}"
+
+
+def COPY_FILE_TO_DIR(src_directory: str, file_name: str, destination_directory: str):
+    if not os.path.exists(f"{destination_directory}/{file_name}"):
+        import shutil
+        shutil.copy(f"{src_directory}/{file_name}", destination_directory)
