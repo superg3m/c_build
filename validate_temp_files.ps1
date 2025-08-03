b5895c80f654e44ac692d71c6312b7da47632010
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-08-03 16:48:40 -0400

    Update validate_temp_files.ps1

--- validate_temp_files.ps1
+++ validate_temp_files.ps1
@@ -7,6 +7,8 @@ $template_content = Get-Content -Path "./c_build/templates/ps1/$current_filename
 $current_content = Get-Content -Path $current_filename
 $differences = Compare-Object -ReferenceObject $current_content -DifferenceObject $template_content
 
+Write-Host "WOW: $current_filename"
+
 if ($differences) {
     Write-Host "Template content for '$current_filename' is out of sync; running ./c_build/bootstrap.ps1" -ForegroundColor yellow
     ./c_build/bootstrap.ps1
